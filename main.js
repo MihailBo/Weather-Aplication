82952641738714cbc8ddcbad61f9c28a9134294a
MihailB <mihail2010borisov@gmail.com>
2026-04-17 15:59:45 +0300

    Added week's program which includes max, min and average temperature and wind speed.

--- main.js
+++ main.js
@@ -26,11 +26,20 @@ async function wind(){
     document.getElementById('wind1').innerHTML = 'Wind <br>' + 'Speed: ' + windSpeed + '<br>' + 'Direction: ' + windDirection
 }
 wind();
-// async function temp(){
-//     const url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m&wind_speed_unit=ms&start_date=2026-03-26&end_date=2026-04-09";
-//     const response = await fetch(url);
-//     const json = await response.json();
-//     const max = json.daily.temperature_2m_max[0];
-//     document.getElementById("day1").innerHTML = "max temp " + max;
-// }
-// temp_day1();
+const date = new Date();
+
+async function week(){
+    const url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min,wind_speed_10m_max,rain_sum";
+    const response = await fetch(url);
+    const json = await response.json();
+    for(let i = 0; i < 7; i++){
+        const maxTemp = json.daily.temperature_2m_max[i];
+        const minTemp = json.daily.temperature_2m_min[i];
+        const midTemp = (maxTemp + minTemp) / 2;
+        document.getElementById(`day${i+1}Temp`).innerHTML = "max temp: " + maxTemp.toFixed(1) + "<br>" +"min temp: " + minTemp.toFixed(1) + "<br>" + "average temp: " + midTemp.toFixed(1);
+        const wind = json.daily.wind_speed_10m_max[i];
+        document.getElementById(`day${i+1}Wind`).innerHTML = "Wind speed: " + wind;
+
+    }
+}
+week();
