b4cec61f689f12090df3a283b39a0e6e3bb9ffa0
MihailBo <mihail2010bprospv@gmail.com>
2026-04-24 15:12:39 +0300

    Added dates.

--- main.js
+++ main.js
@@ -23,11 +23,25 @@ async function wind(){
     const json = await response.json();
     const windSpeed = json.hourly.wind_speed_10m[0];
     const windDirection = json.hourly.wind_direction_10m[0];
-    document.getElementById('wind1').innerHTML = 'Wind <br>' + 'Speed- ' + windSpeed + '<br>' + 'Direction- ' + windDirection
+    document.getElementById('wind1').innerHTML = 'Wind <br>' + 'Speed- ' + windSpeed + "m/s" + '<br>' + 'Direction- ' + windDirection
 }
 wind();
-const date = new Date();
 
+async function dates(){
+
+    for(let i = 0;i < 7; i++){
+        const days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"]
+        const date = new Date();
+        date.setDate(date.getDate() + i);
+        const dayDate = date.getDate();
+        const monthDate = date.getMonth();
+        const yearDate = date.getFullYear();
+        const weekDay = days[date.getDay()];
+
+        document.getElementById(`date${i+1}`).innerHTML =weekDay + "<br>" + dayDate + '.' + monthDate +"." + yearDate;
+    }
+}
+dates();
 async function week(){
     const url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min,wind_speed_10m_max,rain_sum";
     const response = await fetch(url);
@@ -38,8 +52,9 @@ async function week(){
         const midTemp = (maxTemp + minTemp) / 2;
         document.getElementById(`day${i+1}Temp`).innerHTML = "Max- " + maxTemp.toFixed(1) + "°C" + "<br>" +"Min- " + minTemp.toFixed(1) + "°C"  + "<br>" + "Average- " + midTemp.toFixed(1) + "°C" ;
         const wind = json.daily.wind_speed_10m_max[i];
-        document.getElementById(`day${i+1}Wind`).innerHTML = "Wind speed: " + wind + " m/s";
-
+        document.getElementById(`day${i+1}Wind`).innerHTML = "Wind speed: " + wind.toFixed(1) + " m/s";
+        const rain = json.daily.rain_sum[i];
+        document.getElementById(`day${i+1}Rain`).innerHTML = "Rain- " + rain.toFixed(1) + "%";
     }
 }
 week();
